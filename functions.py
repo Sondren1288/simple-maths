5d58d06e333dfbb3e9227a7d5a8baa4e9afae453
Sanket <sanketdonty@gmail.com>
2019-10-10 20:37:45 +0530

    Added function to calculate fibonacci of a number

--- functions.py
+++ functions.py
@@ -142,3 +142,14 @@ def point_distance(point1, point2):
 	"""
 	distance =  math.sqrt( ((point1[0]-point2[0])**2) + ((point1[1]-point2[1])**2) )
 	return distance
+
+def fib(n):
+	"""
+	Returns the nth fibonacci number
+	"""
+	if n < 0:
+		return (fib(n + 2) - fib(n + 1))
+	elif n < 2:
+		return n
+	return fib(n-2) + fib(n-1)
+
