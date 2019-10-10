fdf99c02a18a87f021d14faf7bd0dde591d98cd5
Sami Lahti <sami@JKL17U750.local>
2019-10-10 18:29:03 +0300

    Added Armstrong function

--- functions.py
+++ functions.py
@@ -142,3 +142,18 @@ def point_distance(point1, point2):
 	"""
 	distance =  math.sqrt( ((point1[0]-point2[0])**2) + ((point1[1]-point2[1])**2) )
 	return distance
+
+def armstrong_number(number):
+	"""
+	Check if number is Armstrong number
+	"""
+	calc = number
+	sum = 0
+	while calc > 0:
+		dig = calc % 10
+		sum += dig ** 3
+		calc //= 10
+	if number == sum:
+		return True
+	else:
+		return False
