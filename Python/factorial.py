2d4ee89e28618e207257abd301aeea088cfac12b
tushar-ntiave <tushar.nitave@gmail.com>
2019-10-11 11:09:30 -0500

    implemented factorial using recursion

--- Python/factorial.py
+++ Python/factorial.py
@@ -8,7 +8,9 @@ def factorial(num):
         return 1
 
     # if not, it is the product from 1...num
-    product = 1
-    for integer in range(1, num + 1):
-        product *= integer
-    return product
+    # product = 1
+    # for integer in range(1, num + 1):
+    #     product *= integer
+
+    #implementing recursion
+    return num * factorial(num-1)
