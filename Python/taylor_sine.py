1d17c5afdd6d5e4d34bca64c60b225266aa08f78
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-10-11 09:31:21 +0200

    Update taylor_sine.py

--- Python/taylor_sine.py
+++ Python/taylor_sine.py
@@ -1,16 +1,20 @@
 import math
 
 
-def taylor_sine(x): 
+def taylor_sine(x, accuracy=20): 
     """
     A function to get sine of an angle in radians.
     It uses the taylor expansion of sine to calculate sine.
     Check sine on wikipedia for better explanation.
+
+    Be aware that this loses accuracy fairly quickly.
+    Accuracy can be somewhat increased, but there is a limit
+    to how large integers can be turned to floats.
     """
     multiplier = 1
     result = 0
     
-    for i in range(1,20,2):
+    for i in range(1,accuracy,2):
         result += multiplier*math.pow(x,i)/math.factorial(i)
         multiplier *= -1
     
