3bb1f26e15bffd38f571f78db046a356eefaa094
David del Rio Medina <ddrm86@gmail.com>
2019-10-11 13:04:34 +0200

    Add function to return all divisors of a number

---
+++ Python/find_divisors.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 12:49:26 2019
+
+@author: David del Río Medina
+"""
+
+from math import ceil
+
+def find_divisors(n):
+    """
+    Find all the divisors of a positive number n, including 1 and n.
+    Example: find_divisors(12) -> {1, 2, 3, 4, 6, 12}
+    """
+    divisors = {1}
+    for i in range(2, ceil(n**0.5)+1):
+        if n%i == 0:
+            divisors.add(i)
+            divisors.add(n//i)
+    divisors.add(n)
+    return divisors
