d49a4e6307249ee76f20adefdd13b24ba3c59957
Lseries <44520610+Lseries@users.noreply.github.com>
2019-10-11 17:35:53 +0530

    Update gcd.py
    Made gcd function non-recursive for optimization.

--- Python/gcd.py
+++ Python/gcd.py
@@ -2,6 +2,8 @@ def gcd(n1, n2):
     """
     Recursive function to return the GCD of n1 and n2
     """
-    if n1 == 0:
-        return n2
-    return gcd(n2 % n1, n1)
+    if n1 < n2:
+		n1, n2 = n2, n1
+	while n1 % n2:
+		n1, n2 = n2, n1 % n2
+	return n2
