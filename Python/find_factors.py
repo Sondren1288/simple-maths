c468a2e1bc1af8c3948df43441961fe3061737cc
Vaishnavi Janardhan <vaish.janardhan@gmail.com>
2019-10-11 21:10:41 +0530

    Update find_factors.py

--- Python/find_factors.py
+++ Python/find_factors.py
@@ -2,9 +2,9 @@ def find_factors(n):
     """
     Finds a list of factors of a number
     """
-    factList = []
-    for i in range(1, int(n ** 0.5) + 1):
+    factList = {1, n}
+    for i in range(2, int(n ** 0.5) + 1):
         if (n % i == 0):
-            factList.append(i)
-            factList.append(n // i)
-    return sorted(set(factList))
+            factList.add(i)
+            factList.add(n // i)
+    return sorted(factList)
