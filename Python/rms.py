b9c69bdc5cea7a353bea8253778f93188e248c23
Aakankasha Sharma <akkutbgs3x@gmail.com>
2019-10-12 15:51:22 +0530

    root mean square

---
+++ Python/rms.py
@@ -0,0 +1,14 @@
+import math
+
+def rms(list_):
+	"""
+	finds the root mean square of a list of numbers
+	"""
+
+	result = 0
+	for num in list_:
+		result += num*num
+	
+	result = result / len(list_)
+	result = math.sqrt(result)
+	return result
