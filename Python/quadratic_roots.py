b77746c4456fbd2d487776317931f138f2c4b51c
Akhil Khubchandani <56243241+akhilkh2000@users.noreply.github.com>
2019-10-12 07:28:21 +0530

    updated quadtratic roots.py
    Implemented a function

--- Python/quadratic_roots.py
+++ Python/quadratic_roots.py
@@ -1,21 +1,22 @@
-# Solve the quadratic equation ax**2 + bx + c = 0
-
-import cmath
-
-a = 1
-b = 5
-c = 6
-
-# To take coefficients input from the users
-# a = float(input('Enter a: '))
-# b = float(input('Enter b: '))
-# c = float(input('Enter c: '))
-
-# calculate the discriminant
-d = (b**2) - (4*a*c)
-
-# find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
-
-print('The solution are {0} and {1}'.format(sol1,sol2))
+#funtion to find roots of a quadratic equation 
+def findRoots( a, b, c):    #parameters as coefficients
+  
+    # If a is 0, then equation is  
+    # not quadratic, but linear 
+    if a == 0: 
+        print("Invalid") 
+        return -1
+    d = b * b - 4 * a * c 
+    sqrt_val = math.sqrt(abs(d)) 
+      
+    if d > 0: 
+        print("Roots are real and different ") 
+        print((-b + sqrt_val)/(2 * a)) 
+        print((-b - sqrt_val)/(2 * a)) 
+    elif d == 0: 
+        print("Roots are real and same") 
+        print(-b / (2*a)) 
+    else: #d<0 
+        print("Roots are complex") 
+        print(- b / (2*a) , " + i", sqrt_val) 
+        print(- b / (2*a) , " - i", sqrt_val) 
