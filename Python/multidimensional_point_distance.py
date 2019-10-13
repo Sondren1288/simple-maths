8a564b88cd0082d6b54f9a18910cb9134f704dde
Georgios Giapitzakis Tzintanos <giorgosgiapis189@gmail.com>
2019-10-14 00:03:28 +0300

    Update multidimensional_point_distance.py

--- Python/multidimensional_point_distance.py
+++ Python/multidimensional_point_distance.py
@@ -1,1 +1,10 @@
 import math
+
+def multi_dimensional_distance(point1, point2):
+    """
+    Return the euclidean distance between two points in n-dimensional space 
+    given as tuples.
+    """
+
+    dist_sq = sum([(point1[i] - point2[i])**2 for i in range(len(point1))])
+    return math.sqrt(dist_sq)
