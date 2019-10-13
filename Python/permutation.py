60ba3dc4388c7f348526e100ec2db3fe3482bfaf
Rishabh Sharna <rishabh-327@users.noreply.github.com>
2019-10-13 11:10:04 +0530

    Added Permutation Function

---
+++ Python/permutation.py
@@ -0,0 +1,8 @@
+def permutation(n, r):
+    """
+    Function to calculate permuation of 'n' objects, selected 'r' at a time.
+    """
+    result = 1
+    for i in range(n, n-r, -1):
+        result *= i
+    return result
