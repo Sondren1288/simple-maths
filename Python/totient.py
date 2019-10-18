e4208bb8f06e6026aff3b48750d95a719575af63
RafaelMassoniSS <rafael.massoni@cifraclub.com>
2019-10-18 14:24:56 -0300

    Grammar fix in description in totient.py

--- Python/totient.py
+++ Python/totient.py
@@ -8,7 +8,7 @@ def totient(num):
     This means that if a number 'x' has common divisors with another number 'a' lower than itself,
     it is, 'a' is not relative prime to 'x'.
     This means that if a number is prime, its totient function is its value - 1, as 
-    all numbers lower than the number itself is relative prime to the number.
+    all numbers lower than the number itself are relative prime to the number.
     An example: totient(9):
                 1, 2, 4, 5, 7, 8 are all relative prime
                 3, 6, and 9 have at least one common divisor with 9
