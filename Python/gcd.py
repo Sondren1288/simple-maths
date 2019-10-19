d55701495f45b28da095da41700443fd63d79736
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-10-19 18:32:10 +0200

    Update gcd.py
    Minor formatting

--- Python/gcd.py
+++ Python/gcd.py
@@ -3,7 +3,7 @@ def gcd(n1, n2):
     Non-Recursive function to return the GCD of n1 and n2
     """
     if n1 < n2:
-		n1, n2 = n2, n1
-	while n1 % n2:
-		n1, n2 = n2, n1 % n2
-	return n2
+        n1, n2 = n2, n1
+    while n1 % n2:
+        n1, n2 = n2, n1 % n2
+    return n2
