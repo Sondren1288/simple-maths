6233b7e6b2ead18026bfbba19cd668a431d3ed57
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-10-19 17:58:34 +0200

    Update perfect_square.py
    Changed some formatting

--- Python/perfect_square.py
+++ Python/perfect_square.py
@@ -1,10 +1,12 @@
-# Simple and efficient program to check entered number is a Perfect square
-
 import math
 
 
 def per_sqr(a):
+    """
+    Checks if called number is a perfect square
+    """
     if int(math.sqrt(a) + 0.5) ** 2 == a:
         return True
     else:
         return False
+    
