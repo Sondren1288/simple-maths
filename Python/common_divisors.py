77e248ec1dcbc5c72891068dbfcd444b9f7d3b1b
Sondren1288 <sondrenordmoen@hotmail.com>
2019-10-19 16:57:59 +0200

    Small naming and moving changes

--- Python/common_divisors.py
+++ Python/common_divisors.py
@@ -5,3 +5,10 @@ def gcd(n1, n2):
     if n1 == 0:
         return n2
     return gcd(n2 % n1, n1)
+
+
+def lcm(n1, n2):
+    """
+    Function to return the lcm of two numbers
+    """
+    return (n1 * n2) / gcd(n1, n2)
--- Python/lcm.py
+++
@@ -1,8 +0,0 @@
-from gcd import gcd
-
-
-def lcm(n1, n2):
-    """
-    Function to return the lcm of two numbers
-    """
-    return (n1 * n2) / gcd(n1, n2)
