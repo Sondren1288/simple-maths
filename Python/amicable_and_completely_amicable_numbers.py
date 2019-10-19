1f4725a2e09cb2e6c1f2c04dcd2dd62d2c71e048
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-10-19 16:39:06 +0200

    Update amicable_and_completely_amicable_numbers.py
    Added a description on top to try to explain amicable numbers

--- Python/amicable_and_completely_amicable_numbers.py
+++ Python/amicable_and_completely_amicable_numbers.py
@@ -1,3 +1,13 @@
+"""
+Amicable numbers are numbers where the sum of the diviors equals the other nummber,
+where the divisors would be every number that divides a number n except n itself (includes 1).
+
+An example of amicable numbers:
+(220, 284) 
+They are amicable because the divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110, of which the sum is 284; 
+and the divisors of 284 are 1, 2, 4, 71 and 142, of which the sum is 220. 
+"""
+
 def sum_proper_divisors(x):
     """
     calculate the sum of x's proper divisors
