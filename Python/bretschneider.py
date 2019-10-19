127fb68c200a69e45fc581a9c4cfd437fb43a7c6
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-10-19 16:20:30 +0200

    Update bretschneider.py
    Formatting

--- Python/bretschneider.py
+++ Python/bretschneider.py
@@ -1,7 +1,13 @@
-
 import math
-#calulate the area of a quadrilateral with the length of the four sides a,b,c,d and the degrees of an opposite pair of angles
+
 def bretschneider(a,b,c,d,ab,cd):
-  p=(a+b+c+d)*0.5
-  radsum=(ab+cd)*3.1415926/360
-  return ((p-a)*(p-b)*(p-c)*(p-d)-a*b*c*d*math.cos(radsum)**2)**0.5
+    """
+    Calulate the area of a quadrilateral with the length of the four sides a,b,c,d and 
+    the degrees of an opposite pair of angles.
+    Uses the Bretschneider's formula.
+    Can be used on any quadriliteral as long as all lengths are known
+    and as long as to opposite angles are known.
+    """
+    p=(a+b+c+d)*0.5
+    radsum=(ab+cd)*3.1415926/360
+    return ((p-a)*(p-b)*(p-c)*(p-d)-a*b*c*d*math.cos(radsum)**2)**0.5
