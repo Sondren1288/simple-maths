1f0a78563986383d6b9a330a5df51b37fc9a6f1c
Rick Gleitz <rgleitz@jefflibrary.org>
2019-10-27 20:19:57 -0400

    Fixed typos and renamed a file which had a typo for the name

--- Python/bretschneider.py
+++ Python/bretschneider.py
@@ -5,7 +5,7 @@ def bretschneider(a, b, c, d, ab, cd):
     Calulate the area of a quadrilateral with the length of the four sides a,b,c,d and 
     the degrees of an opposite pair of angles.
     Uses the Bretschneider's formula.
-    Can be used on any quadriliteral as long as all lengths are known
+    Can be used on any quadrilateral as long as all lengths are known
     and as long as to opposite angles are known.
     """
     p = (a + b + c + d) * 0.5
--- Python/triangle.py
+++ Python/triangle.py
@@ -3,7 +3,7 @@ import math
 def area_of_triangle(a, b, c):
     """
     This function applies the heron's formula to calculate
-    the area of traingle, it takes the dimensions of three sides
+    the area of triangle, it takes the dimensions of three sides
     and returns the value of area in respective square dimensions
     """
     s = (a + b + c) / 2 # Semi-perimeter
