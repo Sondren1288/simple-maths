59b0c1a7fd5a7bd5f69e0963eb09cd61d0a62666
Priyanshu24 <priyanshugupta@live.com>
2019-10-31 19:46:00 +0530

    Added a math function for simple interest

---
+++ Python/simple_interest.py
@@ -0,0 +1,10 @@
+def simple_interest(principle, rate, time):
+    '''
+    Calculates simple interest where:
+    1. principle is the amount of money on which simple interest is to be calculated.
+    2. rate is the percentage at which the simple interest is to be calculated.
+    3. time for time period(in years) for which  simple interest is to be calculated
+    '''
+    si = (principle * rate * time) / 100
+    return si
+print(simple_interest(100,2,3))
