46d977ec16c736385d07dc809d7dbee983115fa9
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 19:29:26 +0100

    Moved pythagoras to geometry_basic

--- Python/geometry_basic.py
+++ Python/geometry_basic.py
@@ -34,6 +34,20 @@ def area_triangle(a, b, c):
     return area
 
 
+def pythagoras(p, b):
+    """
+    Returns the hypotenuse given the
+    base and the perpendicular.
+    Returns None if invalid
+    """
+    sq = pow(p, 2) + pow(b, 2)
+    if sq > 0:
+        h = sqrt(sq)
+        return h
+    else:
+        return None
+
+
 # Circles
 def area_circle(radius):
     """
