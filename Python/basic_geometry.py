8befcb34648eaec05b88ca7cfb2969dc43dca040
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 19:12:10 +0100

    Moved cylinders to basic geometry
    Moved cylinder.py to basic geometry and did some formatting

--- Python/basic_geometry.py
+++ Python/basic_geometry.py
@@ -30,7 +30,7 @@ def area_triangle(a, b, c):
     and returns the value of area in respective square dimensions
     """
     s = (a + b + c) / 2 # Semi-perimeter
-    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    area = sqrt(s * (s - a) * (s - b) * (s - c))
     return area
 
 
@@ -40,3 +40,26 @@ def area_circle(radius):
     Calculates the area of a circle given the radius
     """
     return pi * pow(radius,2)
+
+
+# Cylinders
+def area_cylinder(r, h):
+    """
+    Takes in the radius and height of a cylinder
+    and returns the area of the surface of the cylinder
+    """
+    circle = pi * r ** 2
+    # The 2*r is neccessary as we need the diameter
+    body = pi * 2 * r * h
+    area = 2 * circle + body
+    return area
+
+
+def volume_cylinder(r, h):
+    """
+    Finds the volume of a cylinder given 
+    radius and height.
+    """
+    circle = pi * r ** 2
+    volume = circle * h
+    return volume
