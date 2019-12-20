7f22c1ccba160c4d744b601c723654705f8d47d8
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 20:44:58 +0100

    Update and rename taylor_ln_one_plus_x.py to taylor_series.py
    Rename function, improve docstring, change filename

--- taylor_series.py
+++ taylor_series.py
@@ -1,14 +1,16 @@
 import math
 
 
-def taylor_ln_one_plus_x(x, accuracy=20): 
+def ln_one_plus_x(x, accuracy=20): 
     """
     A function to get ln(1+x).
     It uses the taylor expansion.
-    Check sine on wikipedia for better explanation.
+    Check natural logarithm on wikipedia for better explanation.
+    ## https://en.wikipedia.org/wiki/Natural_logarithm ##
     Be aware that this loses accuracy fairly quickly.
     Accuracy can be somewhat increased, but there is a limit
     to how large integers can be turned to floats.
+    ln(1 + x) = x - x^2/2 + x^3/3 - x^4/4...
     """
     multiplier = 1
     result = 0
