17dc03dcda809b57ae1c84b25b5057b30f4d9847
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 20:52:18 +0100

    Update taylor_arctg.py

--- taylor_arctg.py
+++ taylor_arctg.py
@@ -1,14 +1,18 @@
 import math
 
 
-def taylor_arctg(x, accuracy=20): 
+def taylor_arctan(x, accuracy=20): 
     """
-    A function to get arctg.
+    A function to get arctan.
     It uses the taylor expansion.
-    Check sine on wikipedia for better explanation.
+    Check arctan on wikipedia for better explanation.
+    ## https://en.wikipedia.org/wiki/Inverse_trigonometric_functions#arctan ##
+    
     Be aware that this loses accuracy fairly quickly.
     Accuracy can be somewhat increased, but there is a limit
     to how large integers can be turned to floats.
+    
+    arctan(x) = x - x^3/3 + x^5/5 - x^7/7...
     """
     multiplier = 1
     result = 0
