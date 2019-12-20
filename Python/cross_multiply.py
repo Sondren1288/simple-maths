f907739d41ffec0588ef118b3d1f1630e2125fba
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 19:55:00 +0100

    Update cross_multiply.py
    Formatting, docstring

--- Python/cross_multiply.py
+++ Python/cross_multiply.py
@@ -1,4 +1,8 @@
 def cross_multiply(x1:float, x2:float, y1:float):
-	return (y1*x2)/x1
-#	x1	x2
-#	y1	output
+    """
+    Given a fraction that looks like
+    a/b = c/d, returns d = bc/a, as shown below
+    x1	x2
+    y1	output
+    """
+    return (y1 * x2) / x1
