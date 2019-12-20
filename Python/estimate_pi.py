244b6d199e302097b209ecfa45ce984f8da9ec55
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 21:43:35 +0100

    Update estimate_pi.py
    Some docstring changes

--- Python/estimate_pi.py
+++ Python/estimate_pi.py
@@ -1,29 +1,25 @@
-'''
-estimate_pi.py
-
-Estimate the value of Pi
-
-Doing Math with Python by by Amit Saha
-
-'''
 import math
 import random
 
-def estimate(total_points):
+def estimate(total_points=1E5):
+    """
+    Estimates pi by using assuming random points on a square
+    with sides 2 inscribing a circle with radius 1.
+    By dividing the ones in the circle / total point * 4, we get an
+    estimate for pi.
+    
+    Taken from 'Doing Math with Python'by Amit Saha
+    """
     radius = 1
     center = (radius, radius)
 
     in_circle = 0
     for i in range(total_points):
-        x = random.uniform(0, 2*radius)
-        y = random.uniform(0, 2*radius)
+        x = random.uniform(0, 2 * radius)
+        y = random.uniform(0, 2 * radius)
         p = (x, y)
         # distance from circle's center
-        d = math.sqrt((p[0]-center[0])**2 + (p[1]-center[1])**2)
+        d = math.sqrt((p[0] - center[0])**2 + (p[1] - center[1])**2)
         if d <= radius:
             in_circle += 1
-    return (in_circle/total_points)*4
-
-if __name__ == '__main__':
-    for points in [10**3, 10**5, 10**6]:
-        print('Known value: {0}, Estimated ({1}): {2}'.format(math.pi, points, estimate(points)))
+    return (in_circle / total_points) * 4
