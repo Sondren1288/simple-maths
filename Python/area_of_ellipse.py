f59150ff01efe80a1d5b81793022668f7ddc465f
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 20:28:53 +0100

    Delete area_of_ellipse.py
    Removed one of 2 equal functions

--- Python/area_of_ellipse.py
+++
@@ -1,8 +0,0 @@
-from math import pi
-from math import pow
-
-def area_ellipse(half_axis_a,half_axis_b):
-    """
-    Calculates the area of an ellipse
-    """
-    return pi * half_axis_a * half_axis_b
