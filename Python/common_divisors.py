a66f2f5b92195b0a08edd45b5574e003c93f9b48
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 19:38:15 +0100

    Small docstring edit

--- Python/common_divisors.py
+++ Python/common_divisors.py
@@ -1,6 +1,7 @@
 def gcd(n1, n2):
     """
-    Non-Recursive function to return the GCD of n1 and n2
+    Non-Recursive function to return the 
+    GCD (greatest common divisor) of n1 and n2
     """
     if n1 < n2:
         n1, n2 = n2, n1
@@ -11,7 +12,8 @@ def gcd(n1, n2):
 
 def gcd_recursive(n1, n2):
     """
-    Recursive function to return the GCD of n1 and n2
+    Recursive function to return the 
+    GCD (greatest common divisor) of n1 and n2
     """
     if n1 == 0:
         return n2
@@ -20,6 +22,7 @@ def gcd_recursive(n1, n2):
 
 def lcm(n1, n2):
     """
-    Function to return the lcm of two numbers
+    Function to return the lcm 
+    (least common multiple) of two numbers
     """
     return (n1 * n2) / gcd(n1, n2)
