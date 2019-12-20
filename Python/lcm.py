525d3777b3f992f02c962c389c9b13181a4addbf
Sondren1288 <24640870+Sondren1288@users.noreply.github.com>
2019-12-20 21:32:44 +0100

    Update lcm.py

--- Python/lcm.py
+++ Python/lcm.py
@@ -1,11 +1,13 @@
-#least common multiple of two numbers
 def lcm(a,b):
-  greater=b if a<b else a
-  while(True):
-    if((greater%a==0)&(greater%b==0)):
-      lcm=greater
-      break
-    greater+=1
-  return lcm
+    """
+    Finds the least common multiple of 
+    2 numbers without using GCD
+    """
+    greater = b if a < b else a
+    while(True):
+        if ((greater % a == 0) & (greater % b == 0)):
+            lcm=greater
+            break
+        greater+=1
+    return lcm
 
-#print(lcm(5,8))
