23250687321468c1bbe967a7219ee6b85a7f3e9e
Sondren1288 <sondrenordmoen@hotmail.com>
2022-10-05 16:15:52 +0200

    Moved to correct file

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -23,3 +23,19 @@ def fibonacci_recursive_cached(n, _cache={}):
     elif n > 1:
         return _cache.setdefault(n, fibonacci_recursive_cached(n-1) + fibonacci_recursive_cached(n-2))
     return n
+
+
+def fibonacci_dynamic(n):
+    """
+    Dynamic programming using bottom up approach for finding
+    nth fibonacci Number in efficient time
+    """
+    if n == 1 or n == 2:
+        return 1
+    b_up = [None]*40
+    b_up[0] = 0
+    b_up[1] = 1
+    b_up[2] = 2
+    for i in range(2, n+1):
+        b_up[i] = b_up[i-1] + b_up[i-2]
+    return b_up[n]
